410ef95e77d8698102ccabaf551446f4ca8b5703
maualice <alicemauroe@gmail.com>
2022-03-29 18:08:48 -0300

    separando rutas en archivo diferente al principal

---
+++ routes/task.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+//  router.get('/',(req,res)=>{
+//      res.send('all items');
+//     }) 
+ 
+//esta forma permite anidar
+ router.route('/').get((req,res) => {
+    res.send('all items')
+})
+
+module.exports = router
