493e60d2361e993144f94d457dfea7482a9558d3
maualice <alicemauroe@gmail.com>
2022-03-30 18:14:15 -0300

    probando que las rutas funcionen correctamente con postman

--- controllers/task.js
+++ controllers/task.js
@@ -4,11 +4,11 @@ const getAllTasks=(req,res) => {
 }
 
 const createTask=(req,res) => {
-    res.send('create task')
+    res.json(req.body)
 }
 
 const getTask=(req,res) => {
-    res.send('get single task')
+    res.json({ id : req.params.id})
 }
 
 const updateTask=(req,res) => {
--- routes/task.js
+++ routes/task.js
@@ -9,6 +9,6 @@ const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require('../contr
  
 //esta forma permite anidar
  router.route('/').get(getAllTasks).post(createTask)
- router.route('/:id').get(getTask).patch(updateTask).delete(this.deleteTask)
+ router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = router
