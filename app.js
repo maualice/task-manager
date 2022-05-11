d32f04ac85c7c83130453dce11a81efdef344de1
maualice <alicemauroe@gmail.com>
2022-05-10 21:04:30 -0300

    configuracion para produccion

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(notFound); // send 404 if no other route matched
 
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
